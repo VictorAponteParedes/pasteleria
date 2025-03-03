5a702e76456014074b2e22a4d885c6f695c4791c
Victor Aponte <vaponte520@gmail.com>
2025-03-03 14:16:10 -0300

    Actualiza la sección de contacto con nueva información y horarios de atención

--- src/screens/Contact/index.tsx
+++ src/screens/Contact/index.tsx
@@ -8,7 +8,8 @@ const Contact = () => {
       <div className="text-center mb-8">
         <h2 className="text-4xl font-bold ">¡Contáctanos!</h2>
         <p className="mt-4 text-lg text-gray-600">
-          ¿Tienes alguna pregunta o quieres hacer un pedido especial? Estamos aquí para ayudarte.
+          ¿Tienes alguna pregunta o quieres hacer un pedido especial? Estamos
+          aquí para ayudarte.
         </p>
       </div>
 
@@ -16,27 +17,34 @@ const Contact = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 justify-center items-center text-center md:text-left">
         {/* Columna 1: Información de Contacto */}
         <div>
-          <h3 className="text-2xl font-semibold text-pink-400 mb-4">Información de Contacto</h3>
+          <h3 className="text-2xl font-semibold text-pink-400 mb-4">
+            Información de Contacto
+          </h3>
           <div className="flex justify-center md:justify-start items-center mb-4">
             <FaMapMarkerAlt className="text-pink-400 mr-2" />
-            <span className="text-lg text-gray-800">Calle Ficticia 123, Ciudad, País</span>
+            <span className="text-lg text-gray-800">Tenemos delivery</span>
           </div>
           <div className="flex justify-center md:justify-start items-center mb-4">
             <FaPhoneAlt className="text-pink-400 mr-2" />
-            <span className="text-lg text-gray-800">+1 (555) 123-4567</span>
+            <span className="text-lg text-gray-800">(+595) 986 614 340</span>
           </div>
           <div className="flex justify-center md:justify-start items-center mb-4">
             <FaEnvelope className="text-pink-400 mr-2" />
-            <span className="text-lg text-gray-800">contacto@empresa.com</span>
+            <span className="text-lg text-gray-800">
+              salinasgonzalezagata@gmail.com
+            </span>
           </div>
         </div>
 
         {/* Columna 2: Horarios de Atención */}
         <div>
-          <h3 className="text-2xl font-semibold text-pink-400 mb-4">Horarios de Atención</h3>
+          <h3 className="text-2xl font-semibold text-pink-400 mb-4">
+            Horarios de Atención
+          </h3>
           <div className="text-lg text-gray-800">
-            <p>Lunes a Viernes: 9:00 AM - 6:00 PM</p>
-            <p>Sábados: 10:00 AM - 4:00 PM</p>
+            <p>Martes: 9:00 hs - 12:00 HS</p>
+            <p>Jueves: 10:00 AM - 14:00 HS</p>
+            <p>Sabado: 11:00 AM - 13:30 HS</p>
             <p>Domingos: Cerrado</p>
           </div>
         </div>
